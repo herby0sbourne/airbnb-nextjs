e58d102c76acf3f0b26d55b7f2727b7e7e67b9ae
Herby <herbyosbourne31@gmail.com>
2023-07-12 15:48:58 -0500

    create avatar component

---
+++ components/Avatar.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+      <Image src='/images/placeholder.jpeg' alt='avatar' className='rounded-full' width={30} height={30}  />
+  )
+}
+export default Avatar
