ae20306420c995a680772c2e6f39ea6898fd6cfc
Herby <herbyosbourne31@gmail.com>
2023-07-12 15:47:39 -0500

    create Navbar

---
+++ components/Navbar/MenuItem.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+interface MenuItemProps {
+  handleClick:() => void
+  label:string
+
+}
+const MenuItem = ({handleClick,label}:MenuItemProps) => {
+  return (
+      <div className='px-4 py-3 hover:bg-neutral-100 transition font-semibold' onClick={handleClick}>{label}</div>
+  )
+}
+export default MenuItem
