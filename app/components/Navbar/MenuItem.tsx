52516ee43f67797684b01b8487d7fc72dc90d27f
Herby <herbyosbourne31@gmail.com>
2023-07-20 07:35:43 -0500

    prettier format

--- app/components/Navbar/MenuItem.tsx
+++ app/components/Navbar/MenuItem.tsx
@@ -1,13 +1,17 @@
-'use client'
+'use client';
 
 interface MenuItemProps {
-  handleClick:() => void
-  label:string
-
+  handleClick: () => void;
+  label: string;
 }
-const MenuItem = ({handleClick,label}:MenuItemProps) => {
+const MenuItem = ({ handleClick, label }: MenuItemProps) => {
   return (
-      <div className='px-4 py-3 hover:bg-neutral-100 transition font-semibold' onClick={handleClick}>{label}</div>
-  )
-}
-export default MenuItem
+    <div
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+      onClick={handleClick}
+    >
+      {label}
+    </div>
+  );
+};
+export default MenuItem;
