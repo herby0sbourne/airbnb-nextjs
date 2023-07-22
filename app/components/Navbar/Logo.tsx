1e1bf625f94ae44dcbca3fb40a23cff311e4a1c8
Herby <herbyosbourne31@gmail.com>
2023-07-21 19:08:53 -0500

    fix logo not displaying

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import Image from 'next/image';
-import {useRouter} from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 const Logo = () => {
   const router = useRouter();
   return (
     <Image
-      src="/images/logo.svg"
+      src="/images/logo2.svg"
       alt="logo"
       className="hidden md:block cursor-pointer"
       width={100}
