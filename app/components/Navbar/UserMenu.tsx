8dcfec8d94b5141b05c6d689c799dbd46db70be2
Herby <herbyosbourne31@gmail.com>
2023-07-26 21:50:47 -0500

    add function to check if user is signedIn and direct them to loginModal or open RentModal

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -1,15 +1,16 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { User } from '@prisma/client';
-import { signOut } from 'next-auth/react';
-import { AiOutlineMenu } from 'react-icons/ai';
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
-import Avatar from '../Avatar';
-import MenuItem from './MenuItem';
+import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
 
-import useLoginModal from '@/app/hooks/useLoginModal';
-import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useRentModal from "@/app/hooks/useRentModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 interface UserMenuProps {
   currentUser?: User | null;
@@ -18,25 +19,33 @@ interface UserMenuProps {
 function UserMenu({ currentUser }: UserMenuProps) {
   const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
+  const rentModal = useRentModal();
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleOpen = () => {
-    setIsOpen(!isOpen);
-  };
+  const toggleOpen = useCallback(() => {
+    setIsOpen((prevState) => !prevState);
+  }, []);
+
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    // openRentModal
+    rentModal.onOpen();
+  }, [currentUser, loginModal, rentModal]);
 
   return (
     <div className="relative">
       <div className=" flex items-center gap-3">
         <div
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
-          onClick={() => {}}
-        >
+          onClick={onRent}>
           Airbnb your home
         </div>
         <div
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-          onClick={toggleOpen}
-        >
+          onClick={toggleOpen}>
           <AiOutlineMenu />
           <div className="hidden md:block">
             <Avatar imgUrl={currentUser?.image} />
@@ -52,7 +61,7 @@ function UserMenu({ currentUser }: UserMenuProps) {
                 <MenuItem label="My favorite" handleClick={() => {}} />
                 <MenuItem label="My reservations" handleClick={() => {}} />
                 <MenuItem label="My properties" handleClick={() => {}} />
-                <MenuItem label="Airbnb my home" handleClick={() => {}} />
+                <MenuItem label="Airbnb my home" handleClick={rentModal.onOpen} />
                 <hr />
                 <MenuItem label="Logout" handleClick={() => signOut()} />
               </>
