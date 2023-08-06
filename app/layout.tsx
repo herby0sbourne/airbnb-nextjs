f06ec340d9f878065d76f4d53f69671caaca76f7
Herby <herbyosbourne31@gmail.com>
2023-08-06 07:40:01 -0500

    added dummy user

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,43 @@
 import { Nunito } from "next/font/google";
+
 import Navbar from "./components/Navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RentModal from "./components/modals/RentModal";
 import LoginModal from "./components/modals/LoginModal";
 import RegisterModal from "./components/modals/RegisterModal";
+
+import getCurrentUser from "./actions/getCurrentUser";
 import ToasterProvider from "./providers/ToasterProvider";
 
 import "./globals.css";
-import getCurrentUser from "@/app/actions/getCurrentUser";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const currentUser = await getCurrentUser();
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  // const currentUser = await getCurrentUser();
+
+  const currentUser = {
+    id: "vsvsvs",
+    name: null,
+    email: null,
+    emailVerified: null,
+    image: null,
+    hashedPassword: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    favoriteIds: []
+  };
 
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <ToasterProvider />
-        <LoginModal />
-        <RegisterModal />
-        <Navbar currentUser={currentUser} />
+        <ClientOnly>
+          <ToasterProvider />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
         {children}
       </body>
     </html>
