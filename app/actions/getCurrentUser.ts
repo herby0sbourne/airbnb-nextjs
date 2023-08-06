44c09ca6e41bc6e963366d863d9d2e74bbc90fc3
Herby <herbyosbourne31@gmail.com>
2023-08-06 07:40:18 -0500

    remove console log later

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,7 +1,7 @@
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-import prisma from "@/app/libs/prismadb";
+import prisma from '@/app/libs/prismadb';
 
 export async function getSession() {
   return await getServerSession(authOptions);
@@ -10,6 +10,8 @@ export async function getSession() {
 export default async function getCurrentUser() {
   try {
     const session = await getSession();
+
+    console.log('session', session);
 
     if (!session?.user?.email) {
       return null;
