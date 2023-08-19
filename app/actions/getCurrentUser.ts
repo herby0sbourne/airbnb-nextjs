3b2bb157f9f2ed58373571f8cee02648f12e73e3
Herby <herbyosbourne31@gmail.com>
2023-08-19 17:17:43 -0500

    remove console log

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -11,8 +11,6 @@ export default async function getCurrentUser() {
   try {
     const session = await getSession();
 
-    console.log("session", session);
-
     if (!session?.user?.email) {
       return null;
     }
