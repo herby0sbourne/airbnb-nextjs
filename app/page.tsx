056dd32b29aafbd0301012761f12f432b23de52a
Herby <herbyosbourne31@gmail.com>
2023-09-02 18:44:20 -0500

    create ListingCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,39 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
+  title: "Airbnb",
+  description: "Airbnb clone"
 };
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
     <main>
-      <h1 className="text-rose-500 text-2xl">Welcome</h1>
+      {/*<ClientOnly>*/}
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing) => {
+            return <ListingCard key={listing.id} data={listing} currentUser={currentUser} />;
+          })}
+        </div>
+      </Container>
+      {/*</ClientOnly>*/}
     </main>
   );
 }
