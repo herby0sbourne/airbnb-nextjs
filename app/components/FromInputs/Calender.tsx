51ad48b41f1e8e7d7b14f0a02593af02446d583c
Herby <herbyosbourne31@gmail.com>
2023-09-02 13:31:32 -0500

    create datePicker/Calendar component

---
+++ app/components/FromInputs/Calender.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { DateRange, Range, RangeKeyDict } from "react-date-range";
+
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+interface CalenderProps {
+  value: Range;
+  disabledDates?: Date[];
+  onChange: (value: RangeKeyDict) => void;
+}
+
+const Calender = ({ value, disabledDates, onChange }: CalenderProps) => {
+  return (
+    <DateRange
+      rangeColors={["#262626"]}
+      ranges={[value]}
+      date={new Date()}
+      onChange={onChange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={disabledDates}
+    />
+  );
+};
+export default Calender;
