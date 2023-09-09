d015d599b94d0deb893ece047f6cf275ab905771
Herby <herbyosbourne31@gmail.com>
2023-09-09 13:58:33 -0500

    create file to development data

---
+++ data/seedData.ts
@@ -0,0 +1,11 @@
+export const currentUserData = {
+  id: "vsvsvs",
+  name: null,
+  email: null,
+  emailVerified: null,
+  image: null,
+  hashedPassword: null,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  favoriteIds: []
+};
