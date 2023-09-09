99a1e209fbb1916fa35317e8bf5ffe7978d846e0
Herby <herbyosbourne31@gmail.com>
2023-09-09 14:01:38 -0500

    update getReservation comment

--- app/actions/getReservations.ts
+++ app/actions/getReservations.ts
@@ -9,19 +9,16 @@ interface IParams {
 /**
  * This function retrieves reservations from the database based on the provided parameters.
  *
- * @param params - An object that may contain the following properties:
- *   - listingId: The ID of the listing for which reservations are to be retrieved.
- *   - userId: The ID of the user whose reservations are to be retrieved.
- *   - authorId: The ID of the author whose listings' reservations are to be retrieved.
+ * @param {Object} params - An object that may contain the following optional properties:
+ * @param {string} [params.listingId] - The ID of the listing for which reservations are to be retrieved.
+ * @param {string} [params.userId] - The ID of the user whose reservations are to be retrieved.
+ * @param {string} [params.authorId] - The ID of the author whose listings' reservations are to be retrieved.
  *
  * If multiple parameters are provided, they will be used in conjunction to filter the reservations.
- *
  * The function returns an array of reservations, each including its associated listing.
  * The reservations are ordered by their creation date in descending order.
- *
  * @returns {Promise<Array<Object>>} A Promise that resolves to an array of reservations.
- *
- *@throws {Error}  Will throw an error if the database query fails.
+ * @throws {Error}  Will throw an error if the database query fails.
  */
 
 export default async function getReservations(params: IParams) {
