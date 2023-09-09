9c5409dc713fcdf83bfaff1642d6d0347e55a80c
Herby <herbyosbourne31@gmail.com>
2023-09-09 13:58:58 -0500

    create page for trips route

---
+++ app/trips/page.tsx
@@ -0,0 +1,37 @@
+import TripsClient from "./TripsClient";
+import ClientOnly from "../components/ClientOnly";
+import EmptyState from "../components/EmptyState";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+const TripsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="unauthorized" subtitle="please login" />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({ userId: currentUser.id });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState title="No trips found' subtitle='Looks like you have'nt made any reservations" />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <div>
+      <ClientOnly>
+        <TripsClient reservations={reservations} currentUser={currentUser} />
+      </ClientOnly>
+    </div>
+  );
+};
+export default TripsPage;
