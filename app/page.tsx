4b773ec2f30c0fbdd30052eed4f8ae05bdbf50ed
Herby <herbyosbourne31@gmail.com>
2023-09-28 18:21:35 -0500

    update home page searchParams

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
-import getListings from "./actions/getListings";
+import getListings, { IListingsParams } from "./actions/getListings";
 import ListingCard from "./components/listings/ListingCard";
 import getCurrentUser from "./actions/getCurrentUser";
 
@@ -11,8 +11,12 @@ export const metadata: Metadata = {
   description: "Airbnb clone"
 };
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingsParams;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
